c2e1f9e7bd157359fe52282a86e3cee81acf4d35
tiwari-krishna <krishnaptiwari1@gmail.com>
2022-01-31 12:12:09 +0545

    updated dwmblocks scripts

--- config.h
+++ config.h
@@ -1,15 +1,19 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {" 🕉 ", "battery",        5,                      0},
-    {"",          "weather",        3600,                   0},
-    {" ☔ ",   "rain",           3600,                   0},
-    {"",          "moon",           0,                      0},
-    {"",          "wifi",           10,                     0},
-    {"",          "volume",         0,                      10 },
-//	{" 💾 ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	      2,	  	0},
-//    {" 🖥 ", "cpu",  2 ,   0},
-	{" 📅 ", "timedate",		60,		                0},
+    {"", "echo 🕉",        0,                      0},
+//    {"🎵", "mpc | sed -n 1p",        5,                      0},
+    {"", "stat-battery",        5,                      0},
+    {"",          "stat-weather",        3600,                   0},
+    {"",          "stat-temp",        3600,                   0},
+    {"☔",   "stat-rain",           3600,                   0},
+    {"",          "stat-moon",           0,                      0},
+    {"",          "stat-wifi",           10,                     0},
+    {"",          "stat-volume",         0,                     10},
+    {"🔆", "xbacklight | awk '{print int($1)}'",  0 ,   11},
+	{"💾", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	      2,	  	0},
+    {"🖥", "stat-cpu",  2 ,   0},
+	{"📅", "stat-timedate",		60,		                0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
